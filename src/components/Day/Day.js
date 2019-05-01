d38230a4191adb2be3137958bb73a4aa7988f6ab
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-01 17:23:20 +0200

    Added dynamic day display

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Day.css';
-
+import image from '../../assets/images/sunny.png';
 
 const Day = (props) => {
     return (
         <div className="Day">
             <h3>{props.day}</h3>
+            <img src={image} alt="Current weather" />
+            <h4 className="Temperature">20</h4>
         </div>
     )
 }
--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -3,16 +3,56 @@ import Day from '../../components/Day/Day';
 import './Weather.css';
 
 class Weather extends Component {
+
+    state = {
+        dates: [],
+        temperature: []
+    }
+
+    // Weather data provided by APIXU
+    componentDidMount() {
+        fetch('https://api.apixu.com/v1/forecast.json?key=ca7637b4f307488b97a144627190105&q=Bydgoszcz&days=7')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+
+                let dates = [];
+
+                data.forecast.forecastday.map(el => {
+                    return dates.push(el.date);
+                });
+
+                this.setState({
+                    dates: dates
+                });
+            })
+            .catch(error => console.log("Error has occured: ", error));
+    }
+
     render() {
+
+        // Convert numeric dates into string dates for dynamic day display
+        function getDayName(dateStr, locale) {
+            var date = new Date(dateStr);
+            return date.toLocaleDateString(locale, { weekday: 'long' });
+        }
+
+        let days = [];
+
+        this.state.dates.map(el => {
+            return days.push(getDayName(el, "en-US").slice(0, 3) + '.');
+        });
+
         return (
             <div className="Weather">
-                <Day day="Monday"></Day>
-                <Day day="Tuesday"></Day>
-                <Day day="Wednesday"></Day>
-                <Day day="Thursday"></Day>
-                <Day day="Friday"></Day>
-                <Day day="Saturday"></Day>
-                <Day day="Sunday"></Day>
+                <Day day={days[0]}></Day>
+                <Day day={days[1]}></Day>
+                <Day day={days[2]}></Day>
+                <Day day={days[3]}></Day>
+                <Day day={days[4]}></Day>
+                <Day day={days[5]}></Day>
+                <Day day={days[6]}></Day>
             </div >
         );
     }
