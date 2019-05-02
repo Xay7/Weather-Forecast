74c8ba507afbae1fbc13ef683643ee9eb997fc71
Emilian Ćwiok <emilian.cw@gmail.com>
2019-05-02 17:39:34 +0200

    Added multiple days support, changed styles

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -12,12 +12,16 @@ class Weather extends Component {
         loading: true,
         error: false,
         updated: false,
-        weatherData: [],
+        firstDay: [],
         secondDay: [],
         thirdDay: [],
         fourthDay: [],
         fifthDay: [],
-        clickedOnToday: true
+        firstDayClick: true,
+        secondDayClick: false,
+        thirdDayClick: false,
+        fourthDayClick: false,
+        fifthDayClick: false
     }
 
     // Weather data provided by APIXU
@@ -39,7 +43,7 @@ class Weather extends Component {
                 });
 
                 data.forecast.forecastday.map(el => {
-                    return weatherData.temperature.push(el.day.avgtemp_c);
+                    return weatherData.temperature.push(Math.round(el.day.avgtemp_c));
                 });
 
                 data.forecast.forecastday.map(el => {
@@ -72,18 +76,93 @@ class Weather extends Component {
             .then(data => {
 
                 const weatherData = {
-                    weatherData: [],
-                    weeklyTime: []
+                    firstDay: [],
+                    secondDay: [],
+                    thirdDay: [],
+                    fourthDay: [],
+                    fifthDay: [],
+                    weeklyTime: [],
                 }
 
                 data.list.map(el => {
-                    return weatherData.weeklyTime.push(el.dt_txt.slice(11, 16));
+                    return weatherData.weeklyTime.push({
+                        time: (el.dt_txt.slice(11, 16)),
+                        temperature: (Math.round(el.main.temp))
+                    });
                 })
-
+                console.dir(data.list);
+                // Handle first day
                 for (let i = 0; i < 9; i++) {
-                    weatherData.weatherData[i] = { time: weatherData.weeklyTime[i], temperature: Math.round(data.list[i].main.temp) };
+                    weatherData.firstDay[i] = weatherData.weeklyTime[i];
                 }
-                this.setState({ weatherData: weatherData.weatherData });
+
+                // Remove remaining first day hours
+                for (let y = 0; y < 2; y++) {
+                    if (weatherData.weeklyTime[y].time === "03:00" && weatherData.weeklyTime[y + 1].time === "06:00") {
+                        break;
+                    }
+                    else {
+                        y = 0;
+                        weatherData.weeklyTime.shift();
+                    }
+                }
+                // Handle other days
+
+                let help = 0;
+
+                for (let y = 0; y < 9; y++) {
+                    weatherData.secondDay.push(weatherData.weeklyTime[0]);
+                    if (weatherData.weeklyTime[0].time === "00:00" && help > 0) {
+                        help = 0;
+                        break;
+                    }
+                    help++;
+                    weatherData.weeklyTime.shift();
+                }
+
+                for (let y = 0; y < 9; y++) {
+                    weatherData.thirdDay.push(weatherData.weeklyTime[0]);
+                    if (weatherData.weeklyTime[0].time === "00:00" && help > 0) {
+                        help = 0;
+                        break;
+                    }
+                    help++;
+                    weatherData.weeklyTime.shift();
+                }
+
+                for (let y = 0; y < 9; y++) {
+                    weatherData.fourthDay.push(weatherData.weeklyTime[0]);
+                    if (weatherData.weeklyTime[0].time === "00:00" && help > 0) {
+                        help = 0;
+                        break;
+                    }
+                    help++;
+                    weatherData.weeklyTime.shift();
+                }
+
+                for (let y = 0; y < 9; y++) {
+                    weatherData.fifthDay.push(weatherData.weeklyTime[0]);
+                    if (weatherData.weeklyTime[0].time === "00:00" && help > 0) {
+                        help = 0;
+                        break;
+                    }
+                    help++;
+                    weatherData.weeklyTime.shift();
+                }
+
+
+
+
+                console.log(weatherData);
+
+
+                this.setState({
+                    firstDay: weatherData.firstDay,
+                    secondDay: weatherData.secondDay,
+                    thirdDay: weatherData.thirdDay,
+                    fourthDay: weatherData.fourthDay,
+                    fifthDay: weatherData.fifthDay,
+                });
             })
             .catch(error => {
                 console.log("Error has occured: ", error);
@@ -110,12 +189,53 @@ class Weather extends Component {
         this.setState({ error: false });
     }
 
-    onDayClickHandler = () => {
-        this.setState({ clickedOnToday: false })
+    onFirstDayClickHandler = () => {
+        this.setState({
+            firstDayClick: true,
+            secondDayClick: false,
+            thirdDayClick: false,
+            fourthDayClick: false,
+            fifthDayClick: false
+        })
+    }
+    onSecondDayClickHandler = () => {
+        this.setState({
+            firstDayClick: false,
+            secondDayClick: true,
+            thirdDayClick: false,
+            fourthDayClick: false,
+            fifthDayClick: false
+        })
     }
 
-    onTodayClickHandler = () => {
-        this.setState({ clickedOnToday: true })
+    onThirdDayClickHandler = () => {
+        this.setState({
+            firstDayClick: false,
+            secondDayClick: false,
+            thirdDayClick: true,
+            fourthDayClick: false,
+            fifthDayClick: false
+        })
+    }
+
+    onFourthDayClickHandler = () => {
+        this.setState({
+            firstDayClick: false,
+            secondDayClick: false,
+            thirdDayClick: false,
+            fourthDayClick: true,
+            fifthDayClick: false
+        })
+    }
+
+    onFifthDayClickHandler = () => {
+        this.setState({
+            firstDayClick: false,
+            secondDayClick: false,
+            thirdDayClick: false,
+            fourthDayClick: false,
+            fifthDayClick: true
+        })
     }
 
 
@@ -144,43 +264,60 @@ class Weather extends Component {
         // Dynamic data display on graph based on a selected day
         let data = null;
 
-        if (this.state.clickedOnToday) {
-            data = this.state.weatherData;
-        } else data = [{ time: "00:00" }, { time: "03:00" }, { time: "06:00" }, { time: "09:00" }, { time: "12:00" }, { time: "15:00" }, { time: "18:00" }, { time: "21:00" }, { time: "00:00" },]
+        if (this.state.firstDayClick) {
+            data = this.state.firstDay;
+        }
 
+        if (this.state.secondDayClick) {
+            data = this.state.secondDay;
+        }
 
+        if (this.state.thirdDayClick) {
+            data = this.state.thirdDay;
+        }
+        if (this.state.fourthDayClick) {
+            data = this.state.fourthDay;
+        }
+
+        if (this.state.fifthDayClick) {
+            data = this.state.fifthDay;
+        }
 
         const renderLineChart = (
-            <LineChart width={720} height={200} data={data} margin={{ top: 25, right: 50, left: 50, bottom: 5 }} >
-                <Line type="monotone" dataKey="temperature" stroke="#8884d8" isAnimationActive={false}>
-                    <LabelList dataKey="temperature" position="top" offset="15" />
+            <LineChart width={720} height={150} data={data} margin={{ top: 25, right: 50, left: 50, bottom: 5 }} >
+                <Line type="monotone" dataKey="temperature" stroke="#8884d8" isAnimationActive={true} animationDuration={350} animationEasing="ease-in-out">
+                    <LabelList dataKey="temperature" position="top" offset={15} />
                 </Line>
                 <XAxis dataKey="time" axisLine={false} tickLine={false} tick={{ strokeWidth: 5 }} />
             </LineChart>)
 
+
+
         return (
             <Fragment>
+
+                <div>
+                    <input
+                        type="text"
+                        defaultValue="Paris"
+                        className={inputCSS}
+                        onKeyPress={this.onKeyEventHandler}
+                        onBlur={this.onBlurHandler} />
+                </div>
                 <div className="Container">
-                    <div>
-                        <input
-                            type="text"
-                            defaultValue="Paris"
-                            className={inputCSS}
-                            onKeyPress={this.onKeyEventHandler}
-                            onBlur={this.onBlurHandler} />
-                    </div>
                     <div className="Weather">
-                        <Day day={days[0]} temperature={this.state.temperature[0]} icon={this.state.icon[0]} updated={this.state.updated} onclick={this.onTodayClickHandler}></Day>
-                        <Day day={days[1]} temperature={this.state.temperature[1]} icon={this.state.icon[1]} updated={this.state.updated} onclick={this.onDayClickHandler}></Day>
-                        <Day day={days[2]} temperature={this.state.temperature[2]} icon={this.state.icon[2]} updated={this.state.updated} onclick={this.onDayClickHandler}></Day>
-                        <Day day={days[3]} temperature={this.state.temperature[3]} icon={this.state.icon[3]} updated={this.state.updated} onclick={this.onDayClickHandler}></Day>
-                        <Day day={days[4]} temperature={this.state.temperature[4]} icon={this.state.icon[4]} updated={this.state.updated} onclick={this.onDayClickHandler}></Day>
+                        <Day day={days[0]} temperature={this.state.temperature[0]} icon={this.state.icon[0]} updated={this.state.updated} onclick={this.onFirstDayClickHandler}></Day>
+                        <Day day={days[1]} temperature={this.state.temperature[1]} icon={this.state.icon[1]} updated={this.state.updated} onclick={this.onSecondDayClickHandler}></Day>
+                        <Day day={days[2]} temperature={this.state.temperature[2]} icon={this.state.icon[2]} updated={this.state.updated} onclick={this.onThirdDayClickHandler}></Day>
+                        <Day day={days[3]} temperature={this.state.temperature[3]} icon={this.state.icon[3]} updated={this.state.updated} onclick={this.onFourthDayClickHandler}></Day>
+                        <Day day={days[4]} temperature={this.state.temperature[4]} icon={this.state.icon[4]} updated={this.state.updated} onclick={this.onFifthDayClickHandler}></Day>
 
                     </div >
                     <div>
                         {renderLineChart}
-                    </div>
-                </div>
+                    </div></div>
+
+
             </Fragment>
         );
     }
